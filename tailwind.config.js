e476efb5b932205c94a54fc5ec0b35decc575aa7
md-rafi-3-4 <mdrafiislam9411@gmail.com>
2025-01-26 18:17:51 +0600

    Tea-house

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+      boxShadow: {
+        '3xl': '0px 7px 50px 0px rgba(0, 0, 0, 0.6)',}},
+    },
+    plugins: [],
+  }
